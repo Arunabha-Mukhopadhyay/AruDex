b5af4b5b03af2b5223107b4c1228153c1b96610d
Arunabha-Mukhopadhyay <dkarunabha2006@gmail.com>
2026-04-23 16:22:20 +0530

    added sone cors changes

--- Agents/app.py
+++ Agents/app.py
@@ -57,10 +57,11 @@ async def execute_sim(payload:ExecutionRequest) -> ExecutionPlan:
 
 if __name__ == "__main__":
     import uvicorn
+    import os
 
     uvicorn.run(
         "app:app",
         host="0.0.0.0",
-        port=5000,
+        port=int(os.environ.get("PORT", 5000)),
         reload=False,
     )
